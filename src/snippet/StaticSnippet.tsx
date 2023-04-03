c4ae2f77390f08d84128770d5883c10811bcbaa5
Adrian Połubiński <polubis@wp.pl>
2023-04-03 14:56:44 +0200

    Add small refactors

--- src/snippet/StaticSnippet.tsx
+++ src/snippet/StaticSnippet.tsx
@@ -2,18 +2,8 @@ import type { StaticSnippetProps } from "./models";
 import { SnippetContent } from "./SnippetContent";
 
 // This component just renders static snippet.
-const StaticSnippet = ({
-  children,
-  description,
-  header,
-  language
-}: StaticSnippetProps) => (
-  <SnippetContent
-    children={children}
-    description={description}
-    header={header}
-    language={language}
-  />
+const StaticSnippet = (props: StaticSnippetProps) => (
+  <SnippetContent {...props} />
 );
 
 export { StaticSnippet };
