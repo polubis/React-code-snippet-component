1608bb2f159f1999eb4eb14e9097e42fb275952b
Adrian Połubiński <polubis@wp.pl>
2023-04-03 10:48:52 +0200

    Remove snippets

--- snippets/2.tsx
+++
@@ -1,9 +0,0 @@
-interface SnippetProps {
-  children?: string; // Code snippet passed as string.
-  src?: string; // Link to code snippet.
-  description?: string; // Some text displayed under snippet.
-}
-
-const Snippet = ({ children, src, description }: SnippetProps) => {
-  // ...
-};
