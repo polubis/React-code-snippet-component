fd3fd3d4cf66026ee37ebac7cb6d747b968d4f68
Adrian Połubiński <polubis@wp.pl>
2023-04-03 10:39:14 +0200

    Add todos

--- src/snippet/Snippet.tsx
+++ src/snippet/Snippet.tsx
@@ -2,6 +2,10 @@ import { DynamicSnippet } from "./DynamicSnippet";
 import { StaticSnippet } from "./StaticSnippet";
 import { SnippetProps } from "./models";
 
+// TODO: Add error handling for Axios - other ASCII chart
+// Add option to display changes via red bg and green
+// Add option to toggle content inside snippet and be able later to expand it.
+
 const Snippet = ({
   src,
   children,
